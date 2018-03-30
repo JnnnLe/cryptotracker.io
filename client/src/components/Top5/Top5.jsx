f91f1fb02ba172823dc477c145c51be5f7111156
Jennifer Le <jnnnle.sf@gmail.com>
2018-03-29 19:14:41 -0700

    Dynamic Top 5 coins from API

--- client/src/components/Top5/Top5.jsx
+++ client/src/components/Top5/Top5.jsx
@@ -8,25 +8,30 @@ class Top5 extends Component {
   constructor() {
     super();
     this.state = {
-      cryptos: []
+      topCryptos: [],
+      queryParams: '',
     }, 
+
     this.generateCoin = this.generateCoin.bind(this);
     this.generateAllCoins = this.generateAllCoins.bind(this);
   }
 
-  generateCoin(i, coinName, price, PC24Hr) {
+  generateCoin(i, coinName, price, PC24Hr, marketCap) {
     return (
       <div className='Top5' key={i}>
           <div>
             <Row>
-              <Col md={4}>
+              <Col md={3}>
                 {coinName}
               </Col>
-              <Col md={4}>
+              <Col md={3}>
                 $ {price}
               </Col> 
-              <Col md={4}>
+              <Col md={3}>
                 {PC24Hr} %
+              </Col>
+              <Col md={3}>
+                $ {marketCap}
               </Col>
             </Row>
           </div>
@@ -36,24 +41,36 @@ class Top5 extends Component {
 
   generateAllCoins() {
     let coinjsx = [];
-    Object.keys(this.state.cryptos).map((coin, i) => {
-      const coinName = this.state.cryptos[coin].USD.FROMSYMBOL;
-      const price = this.state.cryptos[coin].USD.PRICE;
-      const PC24Hr = (this.state.cryptos[coin].USD.CHANGEPCT24HOUR).toFixed(2);
-      const jsx = this.generateCoin(i, coinName, price, PC24Hr);
-      coinjsx.push(jsx);
-  })
+    Object.keys(this.state.topCryptos).map((coin, i) => {
+      const coinName = this.state.topCryptos[coin].name;
+      const price = this.state.topCryptos[coin].price_usd;
+      const PC24Hr = this.state.topCryptos[coin].percent_change_24h;
+      const marketCap = this.state.topCryptos[coin].market_cap_usd;
+      const jsx = this.generateCoin(i, coinName, price, PC24Hr, marketCap);
 
-  return coinjsx;
-}
+      coinjsx.push(jsx);
+    })
+    return coinjsx;
+  }
 
   componentDidMount() {
-    axios.get('https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,IOT,DMT,LTC,VIU&tsyms=USD')
-      .then(res => {
-        const coins = res.data.RAW;
-          this.setState({ cryptos : coins })
+
+    const arrOfCoinsAbrv = [];
+  
+    axios.get(`https://api.coinmarketcap.com/v1/ticker/?limit=5`)
+    .then(res => {
+      const topCoinsOfTheDayArr = res.data;
+      topCoinsOfTheDayArr.map((coin, i) => {
+        arrOfCoinsAbrv.push(coin.symbol);
       })
-  }
+
+      this.setState({
+        topCryptos: topCoinsOfTheDayArr,
+        queryParams: arrOfCoinsAbrv,
+        
+      })
+    })
+}
 
     render() {
       //gather data
