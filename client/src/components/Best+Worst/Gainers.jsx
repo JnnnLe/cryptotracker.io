2a83ff39406b04cbad5b498b702ee41109a27203
Jennifer Le <jnnnle.sf@gmail.com>
2018-03-30 21:08:12 -0700

    Sorted Top 100 object by 7 day percentsge

--- client/src/components/Best+Worst/Gainers.jsx
+++ client/src/components/Best+Worst/Gainers.jsx
@@ -1,6 +1,7 @@
-import React, { Component, Row, Col } from 'react';
-// import { Col, Row } from 'reactstrap';
+import React, { Component } from 'react';
+import { Col, Row } from 'reactstrap';
 import axios from 'axios';
+import NumberFormat from 'react-number-format';
 
 class Gainers extends Component {
 
@@ -9,15 +10,17 @@ class Gainers extends Component {
     this.state = {
       allCoins: [],
       gainers: [],
-      losers: []
+      // losers: []
     } 
-   
+    this.grabTop100Coins = this.grabTop100Coins.bind(this);
+    this.generateCoin = this.generateCoin.bind(this);
+    this.quick_Sort = this.quick_Sort.bind(this);
   }
 
-// Todo: grab top 5 performers, in the top 100 list
-  generateTopGainers() {
-    let top5jsx = [];
-    let tempjsx = []
+// Todo: grab top 5 performers, in the top 100 list => DONE
+  grabTop100Coins() {
+    let top5jsxFormat = [];
+    // let tempjsx = []
     Object.keys(this.state.allCoins).map((coin, i) => {
       const coinName = this.state.allCoins[coin].name;
       const symbol = this.state.allCoins[coin].symbol;
@@ -25,41 +28,73 @@ class Gainers extends Component {
       const PC7Dy = this.state.allCoins[coin].percent_change_7d;
 
       // const jsx = this.generateCoin(i, coinName, symbol, marketCap, PC7Dy);
-      tempjsx.push({coinName, symbol, marketCap, PC7Dy});
-
-      // const sorted = tempjsx.sort((a,b) => {
-        
-      // })
-
-      // console.log('temp jsx :', tempjsx);
-    })
-    return top5jsx; 
+      top5jsxFormat.push(this.generateCoin(i, coinName, symbol, marketCap, PC7Dy));
+      this.quick_Sort();
+      })
+    return top5jsxFormat; 
   }
 
-  componentDidMount() {
-    axios.get('https://api.coinmarketcap.com/v1/ticker/?limit=0')
+
+  generateCoin(i, coinName, symbol, PC7Dy, marketCap) {
+    // console.log('ALL:', i, coinName, symbol, PC7Dy, marketCap);
+    return (
+      <div className='biggestGainers' key={i}>
+        <div>
+          <Row>
+            <Col md={4}>
+              {coinName} ({symbol})
+            </Col> 
+
+            <Col md={4}>
+            {PC7Dy}
+            </Col>
+
+            <Col md={4}>
+            {marketCap}
+            </Col>
+          </Row>
+       </div>
+      </div>
+    )
+  }
+
+  quick_Sort() {
+    const coins = this.state.allCoins;
+    // console.log('Please be Sorted...', coins)
+      var temp = 0;
+      for (var i = 0; i < coins.length; i++) {
+        for (var j = 0; j < coins.length; j++) {
+          if ((parseInt(coins[i].percent_change_7d)) < (parseInt(coins[j].percent_change_7d))) {
+          temp = coins[i];
+          coins[i] = coins[j];
+          coins[j] = temp;
+        }
+      }
+    }
+    return {coins};
+  }
+  
+  
+  componentWillMount() {
+    axios.get('https://api.coinmarketcap.com/v1/ticker/?limit=100')
       .then(res => {
-        const coins = res.data;
+        const allCoins = res.data;
 
-        
           this.setState({ 
-            allCoins : coins,
-            gainers: this.generateTopGainers(),
-            losers: ['Lowest5']
-            });
-
-          console.log('All coins arr:', this.state.allCoins);
+            allCoins
+          });
       })
   }
 
     render() {
       //gather data
-    //   const coins = this.generateAllCoins();
+      const coins = this.grabTop100Coins();
+      // const sorted = this.quick_Sort();
+
       return (
         <div>
           <div>
-            <h1> Hi there Goregeous!</h1>
-            {this.generateTopGainers()}
+            {coins}
           </div>
         </div>
       )
