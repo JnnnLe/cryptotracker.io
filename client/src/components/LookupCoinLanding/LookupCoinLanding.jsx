e7552cf62a89100bb217140cd776e23744047d70
Jennifer Le <jnnnle.sf@gmail.com>
2018-04-03 14:10:13 -0700

    Style reinstated, yeah!

--- client/src/components/LookupCoinLanding/LookupCoinLanding.jsx
+++ client/src/components/LookupCoinLanding/LookupCoinLanding.jsx
@@ -13,9 +13,9 @@ const LookupCoinLanding =  props => {
   return (
     <div className='userInputForGraph'>
       
-        <form className='rf-search-bar js-search-bar' onSubmit={props.handleSubmit}>
+        <form id='coinInput' className='rf-search-bar js-search-bar' onSubmit={props.handleSubmit}>
           <label>
-            <input className='rf-search-bar__input js-search-bar__input' autoComplete='off' type="text" name='search' value placeholder='Search coin here...' value={userInput} onChange={props.handleInput}/>
+            <input id='coinInput' className='rf-search-bar__input js-search-bar__input' autoComplete='off' type="text" name='search' value placeholder='Search coin here...' value={userInput} onChange={props.handleInput}/>
           </label>
         </form>
 
