f5a797b8a73c3711d0f11e17ced7b59574b703cf
Max Jowett <jowett.max@gmail.com>
2018-04-04 14:50:21 -0700

    Remove one field from modal form

--- client/src/components/AddCoinModal/AddCoinModal.jsx
+++ client/src/components/AddCoinModal/AddCoinModal.jsx
@@ -18,7 +18,6 @@ class AddCoinModal extends React.Component {
   renderModal() {
     return (
       <div>
-        <Button color="danger" onClick={this.props.toggleFn}>{this.props.buttonLabel}</Button>
         <Modal isOpen={this.props.show} toggle={this.props.toggleFn} className={this.props.className}>
           <ModalHeader toggle={this.props.toggleFn}>Add a Coin</ModalHeader>
           
@@ -28,12 +27,9 @@ class AddCoinModal extends React.Component {
           <Input type="text" name="coinName" id="coin-name" placeholder="e.g. BTC" />
           <Label for="quantity">Holdings</Label>
           <Input type="number" name="quantity" id="holdings" placeholder="e.g. 5" />
-          <Label for="price-bought">Price Bought</Label>
-          <Input type="number" name="priceBought" id="price-bought" placeholder="e.g. 100$" />
            
-            <Button type="submit" color="primary">Submit!</Button>{' '}
-            <Button color="secondary" onClick={this.props.toggleFn}>Cancel</Button>
-            <Button color="primary" onClick={this.closeModalRefresh}>DONE!</Button>{' '}
+            <Button type="submit" color="primary">ADD COIN</Button>{' '}
+            <Button color="primary" onClick={this.closeModalRefresh}>DONE</Button>{' '}
             </FormGroup>
             </Form>
           
